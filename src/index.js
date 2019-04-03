13095c50d9b17d7313d387fc7419706bde605bde
linjinghui <852530363@qq.com>
2019-04-03 11:07:16 +0800

    fix: web-base-ui组件模块化

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import './style/main.css';
 import Vue from 'vue';
 import router from './router';
 import App from './App';
-import {install} from 'web-base-ui';
+import {Loading, Tip, Confirm, Prompt} from 'web-base-ui';
 // import './mock.js';
 
 // 注册全局组件
-install(Vue);
+[Loading, Tip, Confirm, Prompt].map(function (component) {
+  Vue.component(component.name, component);
+});
 
 // 用于组件通信
 window.EVENTBUS = new Vue();
